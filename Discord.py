3d7e3a7b42f79073d16d53f058dc59b273a199a3
iLowLife <39988468+iLowLife@users.noreply.github.com>
2022-04-22 21:57:50 +0800

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -4,14 +4,17 @@
 import requests
 
 discordBot_GPA = "https://discord.com/api/webhooks/967055567061418024/Y1UNZnlf6gohtn73C6JJA6B13lB-QmzEbWamJ1REsyL9Wai4gGUgbHA4cK5sejysM4Ne"    #GPA webhook (hackable, but pointless)                                             
-
+                
 headers = {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
 }
 
 message = " UwU "
 payload = {"content": message}
 r = requests.post(discordBot_GPA,data=payload,headers=headers)
+
+
+
 
 
 
